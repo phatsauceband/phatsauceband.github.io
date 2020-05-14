19f49c6f8f36fe61e59d367be8b639361eca891d
MintBics <38803684+MintBics@users.noreply.github.com>
2020-05-14 18:15:17 +1000

    EPK and Contact pages
    along with minor updates to other elements

---
+++ Scripts/scripts.js
@@ -0,0 +1,8 @@
+function openWatch() {
+    document.getElementById("watchNow").style.display = "grid";
+  }
+  
+  /* Close */
+  function closeWatch() {
+    document.getElementById("watchNow").style.display = "none";
+  }
