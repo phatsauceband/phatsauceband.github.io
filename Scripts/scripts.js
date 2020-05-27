37d693a5ef7f221f9af26c12e6dc5b6950bf7d4e
MintBics <38803684+MintBics@users.noreply.github.com>
2020-05-27 22:13:51 +1000

    Addressing list from 21/05
    Key updates include: Watch now fix, refactoring of sass files, integration of Sam's suggestions

--- Scripts/scripts.js
+++
@@ -1,8 +0,0 @@
-function openWatch() {
-    document.getElementById("watchNow").style.display = "grid";
-  }
-  
-  /* Close */
-  function closeWatch() {
-    document.getElementById("watchNow").style.display = "none";
-  }
